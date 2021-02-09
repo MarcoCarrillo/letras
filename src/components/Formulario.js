d237a2fbceebf182934bde30672a2761cf6141af
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-02-08 19:01:15 -0600

    obteniendo la letra de la cancion

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 
 
-const Formulario = () => {
+const Formulario = ({guardarBusquedaLetra}) => {
 
     const [busqueda, guardarBusqueda] = useState({
         artista:'',
@@ -32,10 +32,15 @@ const Formulario = () => {
         }
         guardarError(false);
         //si todo esta bien pasar al componente principal
+        guardarBusquedaLetra(busqueda);
     }
 
     return ( 
         <div className="bg-info">
+            { error ? 
+            <p className="alert alert-danger text-center p2">Todos los campos son obligatorios</p>
+            : null}
+    
             <div className="container">
                 <div className="row">
                     <form
